829d8d6a999276b0c03088d2a9c7485ddd883823
Michalon Loïc <loic.michalon@epita.fr>
2021-08-03 11:51:31 +0200

    First features of the shell, can execute all commands found in PATH as long as it doesn't change the current directory (cd not working yet for example)

---
+++ cmake-build-debug/CMakeFiles/Cshell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cshell.dir/main.c.o"
+  "Cshell"
+  "Cshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 256
+
+char* appendPath(const char* s1, const char* s2){
+    char* ans = malloc(0);
+    int i = 0;
+    for (i = 0; s1[i] != '\0' ; ++i) {
+        ans = realloc(ans, i+1);
+        ans[i] = s1[i];
+    }
+    ans = realloc(ans, i+1);
+    ans[i] = '/';
+    int j = 0;
+    for (j = 0; s2[j] != '\0'; ++j) {
+        ans = realloc(ans, i+j+2);
+        ans[i+j+1] = s2[j];
+    }
+    ans = realloc(ans, i+j+3);
+    ans[i+j+2] = '\0';
+    return ans;
+}
+
+char** splitInput(const char* input){
+    char** ans = malloc(0);
+    char* arg = malloc(0);
+    size_t j = 0;
+    int k = 0;
+    for (int i = 0; !input[i] || input[i] != '\n'; ++i) {
+        if (input[i] == ' '){
+            ans = realloc(ans, (k+1)*sizeof(char*));
+            ans[k] = arg;
+            ++k;
+            j = 0;
+            arg = malloc(0);
+            continue;
+        }
+        arg = realloc(arg, j+1);
+        arg[j] = input[i];
+        ++j;
+    }
+    ans = realloc(ans, k+2);
+    ans[k] = arg;
+    ans[k+1] = NULL;
+    return ans;
+}
+
+char** generatePath(const char* rawPath){
+    char** paths = malloc(0);
+    char* path = malloc(0);
+    int pathSize = 0;
+    int nbPaths = 0;
+    for (int i = 0; rawPath[i] != '\0'; ++i) {
+        if (rawPath[i] == ':'){
+            paths = realloc(paths, (nbPaths+1)*sizeof(char*));
+            paths[nbPaths] = path;
+            path = malloc(0);
+            pathSize = 0;
+            ++nbPaths;
+            continue;
+        }
+        path = realloc(path, pathSize+1);
+        path[pathSize] = rawPath[i];
+        ++pathSize;
+    }
+    paths = realloc(paths, nbPaths+2);
+    paths[nbPaths] = path;
+    paths[nbPaths+1] = NULL;
+    return paths;
+}
+
+int execAllPaths(char** args, char** paths){
+    char* firstArg = *args;
+    if (*firstArg == '/' || *firstArg == '.') return execve(*args, args, __environ);
+    for (int i = 0; paths[i] != NULL; ++i) {
+        *args = appendPath(paths[i], firstArg);
+        int result = execve(*args, args, __environ);
+        if (result != -1) return result;
+    }
+    *args = firstArg;
+    return execve(*args, args, __environ);
+}
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char** argv, char** envp) {
+    char** paths = generatePath(getenv("PATH"));
+    while(1){
+        printf("$ ");
+        fflush(stdout);
+        char userInput[BUFFER_SIZE] = {0};
+        fgets(userInput, BUFFER_SIZE, stdin);
+        if (feof(stdin)) return EXIT_SUCCESS;
+        char** args = splitInput(userInput);
+        pid_t pid = fork();
+        if (pid == 0){
+            //execvp(*args, args);
+            execAllPaths(args, paths);
+        }
+        waitpid(-1, &pid, 0);
+
+    }
+}
